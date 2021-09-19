bcbbea68f20533a28731ccf178ea56736e13d777
harshalpatel <harshal.patel.11@gmail.com>
2021-09-19 11:23:21 -0400

    Created router and initial controller for post

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import helmet from "helmet";
-
+import { pokemonRouter } from "./router/pokemon.route";
 const app = express();
 
 app.use(helmet());
@@ -11,3 +11,5 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+app.use("/", pokemonRouter());
